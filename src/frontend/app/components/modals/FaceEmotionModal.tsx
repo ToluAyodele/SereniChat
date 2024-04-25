e4e5f6ff563b0fe179008d4f7ba6dbd17cbdeed3
laidback_tay <teqwon.norman@outlook.com>
2024-04-25 18:18:22 -0400

    display emotion that is being detected from facial recognition

--- src/frontend/app/components/modals/FaceEmotionModal.tsx
+++ src/frontend/app/components/modals/FaceEmotionModal.tsx
@@ -1,9 +1,10 @@
 import { User } from '@prisma/client';
-import React, { FC, useEffect, useRef } from 'react';
+import React, { FC, useEffect, useMemo, useRef, useState } from 'react';
 import * as faceapi from 'face-api.js';
 import GaugeChart from 'react-gauge-chart';
 
 import Modal from './Modal';
+import { get, set } from 'lodash';
 
 interface FaceEmotionModalProps {
     isOpen: boolean;
@@ -15,18 +16,80 @@ const FaceEmotionModal: FC<FaceEmotionModalProps> = ({ isOpen, onClose }) => {
     const videoRef = useRef<HTMLVideoElement | null>(null);
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-    useEffect(() => {
-        const loadModels = async () => {
-            try {
-                await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
-                await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
-                await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
-                await faceapi.nets.faceExpressionNet.loadFromUri('/models');
-            } catch (err) {
-                console.error("Failed to load models", err);
+    // const [faceEmotionPercentage, setFaceEmotionPercentage] = useState<number>(0.50);
+    const [modelsLoaded, setModelsLoaded] = useState<boolean>(false);
+    // const [gaugeValue, setGaugeValue] = useState<number>(0.50);
+    const [emotion, setEmotion] = useState<string | null>('');
+
+    
+
+    // const consumeFaceEmotions = async (emotions) => {
+    //     await emotions.forEach((emotion, index) => {
+    //         switch(index) {
+    //             case 0:
+    //             case 1:
+    //             case 2:
+    //                 const calculateNegativeEmotions = faceEmotionPercentage + emotion;
+    //                 if (calculateNegativeEmotions > 100) {
+    //                     setFaceEmotionPercentage(100);
+    //                 } else {
+    //                     setFaceEmotionPercentage(calculateNegativeEmotions);
+    //                 }
+    //                 break;
+
+    //             case 3:
+    //                 if (faceEmotionPercentage < 50) {
+    //                     setFaceEmotionPercentage(faceEmotionPercentage + emotion);
+    //                 } else if (faceEmotionPercentage > 50) {
+    //                     setFaceEmotionPercentage(faceEmotionPercentage - emotion);
+    //                 }
+    //                 break;
+
+    //             case 4:
+    //             case 5:
+    //                 const calculatePositiveEmotions = faceEmotionPercentage - emotion;
+    //                 if (calculatePositiveEmotions < 0) {
+    //                     setFaceEmotionPercentage(0);
+    //                 } else {
+    //                     setFaceEmotionPercentage(calculatePositiveEmotions);
+    //                 }
+    //                 break;
+                
+    //             default:
+    //                 break;
+    //         }
+    //     });
+    // };
+
+    const getKeyOfMaxValue = (obj) => {
+        let maxKey: string | null = null;
+        let maxValue = Number.NEGATIVE_INFINITY;
+    
+        for (const [key, value] of Object.entries(obj)) {
+            if (typeof value === 'number' && value > maxValue) {
+                maxKey = key;
+                maxValue = value;
             }
         }
 
+        setEmotion(maxKey);
+    };
+
+
+    useEffect(() => {
+        const loadModels = async () => {
+            try {
+                await Promise.all([
+                    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+                    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+                    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+                    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+                ]);
+            } catch (err) {
+                console.error("Failed to load models", err);
+            }
+        };
+    
         const setUpCamera = async () => {
             if (navigator.mediaDevices.getUserMedia) {
                 const stream = await navigator.mediaDevices.getUserMedia({
@@ -35,19 +98,26 @@ const FaceEmotionModal: FC<FaceEmotionModalProps> = ({ isOpen, onClose }) => {
                 });
                 if (videoRef.current) {
                     videoRef.current.srcObject = stream;
-                    console.log("Video stream set up")
                 }
             }
         }
+    
+        if (isOpen && !modelsLoaded) {
+            loadModels();
+            setUpCamera();
+            setModelsLoaded(true);
+        }
 
+        if (!isOpen) {
+            setModelsLoaded(false);
+        }
+
+    }, [isOpen, modelsLoaded]);
+
+    useEffect(() => {
         const startFaceDetection = async () => {
             const canvas = canvasRef.current;
             if (!canvas) {
-                return;
-            }
-    
-            const context = canvas.getContext('2d');
-            if (!context) {
                 return;
             }
             
@@ -55,9 +125,11 @@ const FaceEmotionModal: FC<FaceEmotionModalProps> = ({ isOpen, onClose }) => {
                 if (videoRef.current && canvasRef.current) {
                     const detections = await faceapi.detectAllFaces(videoRef.current,
                         new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-    
-                    canvasRef.current.replaceWith(canvas);
-                
+                    
+                    if (detections.length === 0 || !detections[0].expressions) {
+                        return;
+                    }
+
                     faceapi.matchDimensions(canvas, {
                         width: canvas.width,
                         height: canvas.height
@@ -72,14 +144,14 @@ const FaceEmotionModal: FC<FaceEmotionModalProps> = ({ isOpen, onClose }) => {
                     faceapi.draw.drawFaceLandmarks(canvas, resized);
                     faceapi.draw.drawFaceExpressions(canvas, resized);
 
+                    getKeyOfMaxValue(detections[0].expressions);
                 }
             }, 100);
         };
         
-        loadModels();
-        setUpCamera();
         startFaceDetection();
     });
+
 
     return (
         <>  
@@ -94,16 +166,20 @@ const FaceEmotionModal: FC<FaceEmotionModalProps> = ({ isOpen, onClose }) => {
                         style={{ 'height' : '550px'}}
                         className='absolute top-12 z-0'>
                     </video>
+                    <div className="text-white">
+                        { emotion }
+                    </div>
                 </div>
-                <GaugeChart
+                {/* <GaugeChart
                     id="gauge-chart5"
                     nrOfLevels={10}
                     arcsLength={[0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]}
                     colors={['#5BE12C', '#F5CD19', '#EA4228']}
-                    percent={0.37}
+                    percent={Math.round(gaugeValue * 100) / 100}
                     arcPadding={0.02}
+                    animate={false}
                     style={{ 'height' : '250px' ,'marginTop' : '650px'}}
-                />
+                /> */}
             </Modal>
         </>
     );
